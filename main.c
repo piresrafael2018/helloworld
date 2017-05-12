51a38e743754fc2dd5c0d2afc9e7858f9aea4a80
Rafael Pires <piresrafael2011@hotmail.com>
2017-05-12 10:45:01 -0300

    seccond commit

---
+++ main.c
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: suporte
+ *
+ * Created on 5 de Maio de 2017, 09:37
+ */
+#include <stdio.h>
+#include "newfile.c"
+
+ int main(void)
+ {
+    executar();
+    return 0;
+ }
+ 
+ 
+ void helloWorld(){
+     printf("Hello World\n");
+ }
